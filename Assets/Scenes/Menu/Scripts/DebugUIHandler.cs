8a4d0693197f012899d58da7d8deebadb7fb89c9
flo <f.klein@protonmail.com>
2021-02-10 11:32:55 +0100

    implemented debug log messages on screen

---
+++ Assets/Scenes/Menu/Scripts/DebugUIHandler.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DebugUIHandler : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_Text _text = null;
+
+    private static DebugUIHandler _instance;
+
+    private string _log;
+    private Queue<string> _logQueue = new Queue<string>();
+
+    private void Start()
+    {
+        Debug.Log("Test Message 1");
+        Debug.Log("Test Message 2");
+        Debug.Log("Test Message 3");
+        Debug.Log("Test Message 4");
+    }
+
+    private void Awake()
+    {
+        // singleton logic
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string msg, string stackTrace, LogType type)
+    {
+        string color;
+
+        switch (type)
+        {
+        case LogType.Log:
+            color = "white";
+            break;
+        case LogType.Warning:
+            color = "yellow";
+            break;
+        case LogType.Error:
+            color = "red";
+            break;
+        default:
+            color = "white";
+            break;
+        }
+
+        string log = "<color=" + color + ">[" + type + "] " + msg + "</color=" + color + ">\n";
+
+        _logQueue.Enqueue(log);
+        _log = string.Empty;
+
+        foreach (string item in _logQueue)
+        {
+            _log += item;
+        }
+
+        _text.text = _log;
+    }
+}
